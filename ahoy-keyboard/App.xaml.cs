0dbb815f7fc1b70e2b52090fae71ba21ece3e03a
김수영 <suyoung.kim@fescaro.com>
2024-08-27 18:29:08 +0900

    Add project files.

---
+++ ahoy-keyboard/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ahoy_keyboard
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
